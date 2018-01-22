cca71ff8335bcabf90febb59c34a822634900bdf
Marly <marlycormar@gmail.com>
2018-01-22 16:39:45 -0500

    Improve readability

--- js/addText.js
+++ js/addText.js
@@ -3,9 +3,13 @@ $('document').ready(function() {
 
   // Find the table containing message 'What is the REDCap Mobile App?''
   $table = $("tbody:has(td:contains('What is the REDCap Mobile App?'))");
+
+  // Prepend text provided by user
   $table.prepend("<tr><td>"+routeMobileApiRequestText+"</td></tr>");
   $table.prepend("<tr><td><div><p> </p></div></td></tr>");
 
+  // Modify target URL for 'Request API Token' button 
+  // TODO
     
 });
 
