729d0d210259d7e127ad389b7bcf02a70a3b4f34
Marly <marlycormar@gmail.com>
2018-01-22 16:36:23 -0500

    Add comment

--- js/addText.js
+++ js/addText.js
@@ -1,6 +1,7 @@
 $('document').ready(function() {
 
 
+  // Find the table containing message 'What is the REDCap Mobile App?''
   $table = $("tbody:has(td:contains('What is the REDCap Mobile App?'))");
   $table.prepend("<tr><td>"+routeMobileApiRequestText+"</td></tr>");
   $table.prepend("<tr><td><div><p> </p></div></td></tr>");
