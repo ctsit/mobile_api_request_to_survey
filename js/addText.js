ade4882fce3bcfb2d90129cf5daa523ea7cac750
Marly <marlycormar@gmail.com>
2018-01-22 16:15:23 -0500

    Add js file that handles the addition of formatted text

---
+++ js/addText.js
@@ -0,0 +1,11 @@
+$('document').ready(function() {
+
+
+  $table = $("tbody:has(td:contains('What is the REDCap Mobile App?'))");
+  $table.prepend("<tr><td>"+routeMobileApiRequestText+"</td></tr>");
+  $table.prepend("<tr><td><div><p> </p></div></td></tr>");
+
+    
+});
+
+
