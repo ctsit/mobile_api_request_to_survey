ea4a4935f06869ba82a6a80279211b9ecdcef324
Marly <marlycormar@gmail.com>
2018-01-23 13:32:36 -0500

    Check that both the table and button exist

--- js/addTextAndURL.js
+++ js/addTextAndURL.js
@@ -3,11 +3,18 @@ $('document').ready(function() {
 	$table = $("tbody:has(td:contains('What is the REDCap Mobile App?'))");
 
 	// Prepend text provided by user
-	$table.prepend("<tr><td>"+mobileApiRequestToSurveyText+"</td></tr>");
-	$table.prepend("<tr><td><div><p> </p></div></td></tr>");
+	if($table.length == 1) {
+		$table.prepend("<tr><td>"+mobileApiRequestToSurveyText+"</td></tr>");
+		$table.prepend("<tr><td><div><p> </p></div></td></tr>");
+	}
+
+	// Find the 'Request API Token' button
+	$requestTokenButton = $("button:has(span:contains('Request API token'))");
 
 	// Modify target URL for 'Request API Token' button
-	$('.jqbuttonmed').attr("onclick","window.location='"+mobileApiRequestToSurveyURL+"'");
+	if ($requestTokenButton.length == 1) {
+		$requestTokenButton.attr("onclick","window.location='"+mobileApiRequestToSurveyURL+"'");
+	}
 });
 
 
