f4a324909e0cf81509cc65e43af658938981fc88
Marly <marlycormar@gmail.com>
2018-01-23 15:09:29 -0500

    Modify comment

--- js/addTextAndURL.js
+++ js/addTextAndURL.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	// Find the table containing message 'What is the REDCap Mobile App?''
+	// Find the table containing message 'What is the REDCap Mobile App?'
 	$table = $("tbody:has(td:contains('What is the REDCap Mobile App?'))");
 
 	// Prepend text provided by user
