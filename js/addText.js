65b8b11ca814b04697a312a5ea36ae891f727d37
Marly <marlycormar@gmail.com>
2018-01-23 11:10:33 -0500

    Change variable names to match new module name

--- js/addText.js
+++ js/addText.js
@@ -1,16 +1,14 @@
 $('document').ready(function() {
 
+	// Find the table containing message 'What is the REDCap Mobile App?''
+	$table = $("tbody:has(td:contains('What is the REDCap Mobile App?'))");
 
-  // Find the table containing message 'What is the REDCap Mobile App?''
-  $table = $("tbody:has(td:contains('What is the REDCap Mobile App?'))");
+	// Prepend text provided by user
+	$table.prepend("<tr><td>"+mobileApiRequestToSurveyText+"</td></tr>");
+	$table.prepend("<tr><td><div><p> </p></div></td></tr>");
 
-  // Prepend text provided by user
-  $table.prepend("<tr><td>"+routeMobileApiRequestText+"</td></tr>");
-  $table.prepend("<tr><td><div><p> </p></div></td></tr>");
-
-  // Modify target URL for 'Request API Token' button 
-  // TODO
-    
+	// Modify target URL for 'Request API Token' button 
+	// TODO
 });
 
 
